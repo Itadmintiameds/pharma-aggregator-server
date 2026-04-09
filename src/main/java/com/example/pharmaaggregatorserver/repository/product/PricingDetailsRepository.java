4235546b4b9f0e3938c9d463f4621a319162c82b
ShreerakshaRao <your_github_shreeraksha@tiameds.ai>
2026-04-09 09:43:21 +0530

    Redeploying as the previous deployment failed

--- src/main/java/com/example/pharmaaggregatorserver/repository/product/PricingDetailsRepository.java
+++ src/main/java/com/example/pharmaaggregatorserver/repository/product/PricingDetailsRepository.java
@@ -1,10 +1,11 @@
 package com.example.pharmaaggregatorserver.repository.product;
 
+import com.example.pharmaaggregatorserver.entity.product.PricingDetails;
 import com.example.pharmaaggregatorserver.entity.product.ProductDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface PricingDetailsRepository extends JpaRepository<ProductDetails, String>{
+public interface PricingDetailsRepository extends JpaRepository<PricingDetails, String>{
 
     @Query(value = """
     SELECT MAX(CAST(SUBSTRING(pricing_id, LENGTH(pricing_id) - 4, 5) AS INTEGER))
