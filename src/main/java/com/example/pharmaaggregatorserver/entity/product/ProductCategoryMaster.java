0fe4bc9f5797a95230af751e036e80b42cf8710b
ShreerakshaRao <your_github_shreeraksha@tiameds.ai>
2026-04-29 14:26:52 +0530

    Bug sheet fixes

---
+++ src/main/java/com/example/pharmaaggregatorserver/entity/product/ProductCategoryMaster.java
@@ -0,0 +1,35 @@
+package com.example.pharmaaggregatorserver.entity.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tm_product_category_master")
+public class ProductCategoryMaster {
+
+    @Id
+    @Column(name = "product_category_id")
+    private Long productCategoryId;
+
+    @Column(name = "product_category")
+    private String productCategory;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @JsonIgnore
+    private Category category;
+
+    @OneToMany(mappedBy = "productCategoryMaster", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private Set<ProductSubcategoryMaster> productSubcategoryMasters;
+}
