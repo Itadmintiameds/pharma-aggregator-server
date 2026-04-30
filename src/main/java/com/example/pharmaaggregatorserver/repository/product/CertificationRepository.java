1a9d74729d094536f063c25695f31710ca7da6ee
basavarajunn <basavarajunn@tiameds.ai>
2026-04-30 09:40:58 +0530

    Update certification to support universal master table

--- src/main/java/com/example/pharmaaggregatorserver/repository/product/CertificationRepository.java
+++ src/main/java/com/example/pharmaaggregatorserver/repository/product/CertificationRepository.java
@@ -3,8 +3,11 @@ package com.example.pharmaaggregatorserver.repository.product;
 import com.example.pharmaaggregatorserver.entity.product.MedicalDeviceProductMaster.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
     Optional<Certification> findByCertificationName(String trimmed);
+
+    List<Certification> findByCategory_CategoryId(Long categoryId);
 }
