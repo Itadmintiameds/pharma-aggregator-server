436ef06b5e895846a185d68a7c8204c76eb2e6c2
basavarajunn <basavarajunn@tiameds.ai>
2026-05-08 18:13:04 +0530

    Added import product for supplements/nutraceuticals

--- src/main/java/com/example/pharmaaggregatorserver/repository/product/DosageFormRepository.java
+++ src/main/java/com/example/pharmaaggregatorserver/repository/product/DosageFormRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -12,5 +13,5 @@ public interface DosageFormRepository extends JpaRepository<DosageForm, Long> {
 
     List<DosageForm> findByCategory_CategoryId(Long categoryId);
 
-
+    Optional<DosageForm> findByDosageNameAndCategory_CategoryId(String dosageName, Long categoryId);
 }
--- src/main/java/com/example/pharmaaggregatorserver/repository/product/FlavourRepository.java
+++ src/main/java/com/example/pharmaaggregatorserver/repository/product/FlavourRepository.java
@@ -3,5 +3,9 @@ package com.example.pharmaaggregatorserver.repository.product;
 import com.example.pharmaaggregatorserver.entity.product.Flavour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FlavourRepository extends JpaRepository<Flavour, Long> {
+
+    Optional<Flavour> findByFlavourName(String flavourName);
 }
