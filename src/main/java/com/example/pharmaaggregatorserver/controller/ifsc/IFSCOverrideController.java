56f04b62196f35b513f7d5865e72f15cd9e317e6
dipakgadadu <dipakdagadu@tiameds.ai>
2026-05-14 17:56:44 +0530

    Added ifsc code

--- src/main/java/com/example/pharmaaggregatorserver/controller/ifsc/IFSCOverrideController.java
+++ src/main/java/com/example/pharmaaggregatorserver/controller/ifsc/IFSCOverrideController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/ifsc")
+@RequestMapping("/ifsc")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class IFSCOverrideController {
